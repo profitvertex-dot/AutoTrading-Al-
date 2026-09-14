0082167007fa4aa2ac85f613e945142e6494f3af
profitvertex-dot <profitvertex@gmail.com>
2026-09-14 14:41:06 +0530

    Update ticker.js

--- api/ticker.js
+++ api/ticker.js
@@ -1,5 +1,7 @@
 export default async function handler(req,res){
-  const r = await fetch('https://public.coindcx.com/market_data/current_prices');
-  const data = await r.json();
-  res.json(data);
+  try{
+    const r = await fetch('https://public.coindcx.com/market_data/current_prices');
+    const data = await r.json();
+    return res.status(200).json(data);
+  }catch(e){ return res.status(200).json({BTCINR:"0"}); }
 }
