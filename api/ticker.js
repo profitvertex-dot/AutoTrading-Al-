7bb6e7446e957c0d6b8f9070b9345e61e0c3efbb
profitvertex-dot <profitvertex@gmail.com>
2026-09-14 15:44:02 +0530

    Update ticker.js

--- api/ticker.js
+++ api/ticker.js
@@ -1,10 +1,6 @@
-export default async function handler(req, res){
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  try{
-    const r = await fetch('https://api.coindcx.com/exchange/ticker');
-    const data = await r.json();
-    res.json(data);
-  }catch(e){
-    res.status(500).json({error: e.message});
-  }
+export default async function handler(req,res){
+ res.setHeader('Access-Control-Allow-Origin','*');
+ const r=await fetch('https://api.coindcx.com/exchange/ticker');
+ const data=await r.json();
+ res.json(data);
 }
