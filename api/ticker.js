0cd51072f721d70fd9a6f4b75268d8a4f6e8f0e0
profitvertex-dot <profitvertex@gmail.com>
2026-09-15 13:56:27 +0530

    Update ticker.js

--- api/ticker.js
+++ api/ticker.js
@@ -1,10 +1,8 @@
 export default async function handler(req,res){
-  res.setHeader('Access-Control-Allow-Origin','*');
   try{
-    let r=await fetch('https://public.coindcx.com/exchange/ticker');
-    let d=await r.json(); res.status(200).json(d);
-  }catch(e){
-    let cg=await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=inr').then(x=>x.json());
-    res.status(200).json([{market:'BTCINR',last_price:cg.bitcoin.inr}]);
-  }
+    let r = await fetch('https://api.coindcx.com/exchange/ticker');
+    let j = await r.json();
+    res.setHeader('Access-Control-Allow-Origin','*');
+    return res.json(j);
+  }catch(e){ return res.json({error:e.message}); }
 }
