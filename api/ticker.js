61206213708c37d952445ae528217e1609aa54d8
profitvertex-dot <profitvertex@gmail.com>
2026-09-16 13:57:15 +0530

    Update ticker.js

--- api/ticker.js
+++ api/ticker.js
@@ -1,8 +1,6 @@
 export default async function handler(req,res){
-  try{
-    let r = await fetch('https://public.coindcx.com/market_data/ticker');
-    let j = await r.json();
-    res.setHeader('Access-Control-Allow-Origin','*');
-    return res.json(j);
-  }catch(e){ return res.json([]); }
+  let r = await fetch('https://public.coindcx.com/market_data/ticker');
+  let j = await r.json();
+  res.setHeader('Access-Control-Allow-Origin','*');
+  return res.json(j);
 }
