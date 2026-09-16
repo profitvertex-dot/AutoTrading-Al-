f70af2fed0340fbd01becbb56ddda3c3b416f610
profitvertex-dot <profitvertex@gmail.com>
2026-09-16 14:10:02 +0530

    Update candles.js

--- api/candles.js
+++ api/candles.js
@@ -1,18 +1,17 @@
 export default async function handler(req,res){
+  res.setHeader('Access-Control-Allow-Origin','*');
   try{
     let interval = req.query.interval || '15m';
-    // SHIB ka data
     let r = await fetch(`https://public.coindcx.com/market_data/candles?pair=I-SHIB_INR&interval=${interval}&limit=100`);
-    let j = await r.json();
-    if(!j || j.length==0){
-      // agar SHIB fail toh BTC fallback
+    let data = await r.json();
+    
+    // Agar SHIB na aaye toh BTC fallback
+    if(!data || !data.length){
       r = await fetch(`https://public.coindcx.com/market_data/candles?pair=I-BTC_INR&interval=${interval}&limit=60`);
-      j = await r.json();
+      data = await r.json();
     }
-    res.setHeader('Access-Control-Allow-Origin','*');
-    return res.json(j.reverse());
-  }catch(e){ 
-    res.setHeader('Access-Control-Allow-Origin','*');
-    return res.json([]); 
+    return res.status(200).json(data);
+  }catch(e){
+    return res.status(200).json([]);
   }
 }
